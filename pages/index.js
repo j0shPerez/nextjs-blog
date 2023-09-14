57c0d3c474db45c6bb118ff22e4ca2f6c40e4e78
j0shPerez <124333738+j0shPerez@users.noreply.github.com>
2023-09-14 14:11:37 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -23,10 +23,7 @@ export default function Home() {
 <br>10. Having A Strong Work Ethic</br></p>
 
     </ul>
-    <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
+  
       </section>
     </Layout>
   );
