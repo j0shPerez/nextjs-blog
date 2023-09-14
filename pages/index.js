0d66d9e937132b441b3e1bc50209ea02e842662c
j0shPerez <124333738+j0shPerez@users.noreply.github.com>
2023-09-14 13:55:54 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -9,20 +9,18 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hi my name is Josh Nathaniel Perez, I am now a 3rd year Computer engineering student
-At the University of the Assumption. My other hobbies are playing basketball and riding a motorcycles and as of now i joined  2 basketball leagues in our barangay. I take computer engineering because now a days computer engineering is in demand now thats why i take this course. Now iwant to learn more about ths course because 5 years from now i want to see my self as a successful computer engineer. 
-10 Things That Require Zero Talent
+        <p>10 Things That Require Zero Talent
 
-1. Being On Time
-2. Making An Effort
-3. Being High Energy
-4. Having A Positive Attitude
-5. Being Passionate
-6. Using Good Body Language
-7. Being Coachable
-8. Doing A Little Extra
-9. Being Prepared
-10. Having A Strong Work Ethic
+<br>1. Being On Time</br>
+<br>2. Making An Effort</br>
+<br>3. Being High Energy</br>
+<br>4. Having A Positive Attitude</br>
+<br>5. Being Passionate</br>
+<br>6. Using Good Body Language</br>
+<br>7. Being Coachable</br>
+<br>8. Doing A Little Extra</br>
+<br>9. Being Prepared</br>
+<br>10. Having A Strong Work Ethic</br>
   </p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
